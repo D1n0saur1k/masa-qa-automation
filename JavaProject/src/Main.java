59ebeb2cd690070e8e6f7ef4abb9720e651434f3
D1n0saur1k <dlevit2003@gmail.com>
2022-12-19 19:18:18 +0200

    com3

--- JavaProject/src/Main.java
+++ JavaProject/src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("huita eta vasha java");
+        System.out.println("Давай поговорим о твоём одиночестве");
+        System.out.println("Никого кругом, листья падают в бассейн");
+        System.out.println("Сотни телефонных номеров, да всё не те");
+        System.out.println("Другом быть легко, никто не подавал пример");
     }
 }
