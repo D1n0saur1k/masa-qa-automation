f4668e7f56244592edd244461c9ba6e2c49dc4a6
D1n0saur1k <dlevit2003@gmail.com>
2022-12-26 19:38:24 +0200

    comm7

---
+++ JavaProject/src/lesson3_1/IfElse.java
@@ -0,0 +1,24 @@
+package lesson3_1;
+
+import java.util.Scanner;
+public class IfElse {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please Enter Your Age... ");
+        int age = input.nextInt();
+        if (age > 18) {
+            System.out.println("U'r Adult");
+        }
+
+        else if ( age==18 ) {
+            System.out.println("Idi pospi");
+
+        }
+
+        else {
+            System.out.println("U'r too young");
+        }
+    }
+}
