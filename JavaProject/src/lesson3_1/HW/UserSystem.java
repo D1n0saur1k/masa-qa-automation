332e06b1007ebea36ef342699ce84b29a9c2a6c9
D1n0saur1k <dlevit2003@gmail.com>
2022-12-27 19:15:20 +0200

    comm12

---
+++ JavaProject/src/lesson3_1/HW/UserSystem.java
@@ -0,0 +1,41 @@
+package lesson3_1.HW;
+import java.util.Scanner;
+public class UserSystem {
+    public static void main(String[] args) {
+        String username = "Admin";
+        String password = "P@ssword";
+
+        System.out.println("Enter your username: ");
+
+        Scanner input = new Scanner(System.in);
+
+        String checker;
+        do {
+           checker = input.next();
+            if (!username.equals(checker)) {
+                System.out.println("Wrong username, please try again ");
+            }
+        } while (!username.equals(checker));
+        System.out.println("Enter your password");
+        do {
+            checker = input.next();
+            if (!password.equals(checker)) {
+                System.out.println("Wrong password, please try again ");
+            }
+        } while (!password.equals(checker));
+        System.out.println("Hi " + username + " U're In");
+        input.close();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
